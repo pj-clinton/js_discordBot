a7b3e72d6f873dfccdf8c81f57e2569200c50e03
PJ Clinton <github@intheory.bulc.club>
2022-12-02 00:23:41 -0600

    Inital commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN)
